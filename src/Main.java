2cb9ec70eefee53994b8e0cc48f8c29264b2e409
DenKor29 <denis.p.korablev@gmail.com>
2019-06-13 14:58:05 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         CommunicationDevice communicationDevice = new CommunicationDevice(0);
         Comutator comutator = new Comutator("Comutator",100);
         Modem modem = new Modem("Modem",1000);
-        Router router = new Router("Router",1000)
+        Router router = new Router("Router",1000);
     }
 }
